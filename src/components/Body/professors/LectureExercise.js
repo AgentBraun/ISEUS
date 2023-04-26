e9aae35e466b33a27dc5d95332af876ddcc0e49b
AgentBraun <AgentBraun@users.noreply.github.com>
2023-04-26 16:04:48 +0200

    Exercises page done

--- src/components/Body/professors/LectureExercise.js
+++ src/components/Body/professors/LectureExercise.js
@@ -5,6 +5,8 @@ import { faFileArrowDown, faQrcode } from '@fortawesome/free-solid-svg-icons';
 import classes from './LectureExercise.module.css';
 
 const LectureExamLine = (props) => {
+  const toggleHandler = () => {};
+
   const data = props.info;
   return (
     <div className={classes.lecture}>
@@ -13,11 +15,14 @@ const LectureExamLine = (props) => {
       <p className={classes.lectureDuration}>{data.duration}</p>
       <p className={classes.lectureDate}>{data.date}</p>
       <div className={classes.lectureScan}>
-        <ButtonToggle></ButtonToggle>
+        <ButtonToggle onClick={toggleHandler} toggled={true}></ButtonToggle>
       </div>
       <div className={classes.qr}>
         <FontAwesomeIcon className={classes.icon} icon={faFileArrowDown} />
-        <FontAwesomeIcon className={classes.icon} icon={faQrcode} />
+        <FontAwesomeIcon
+          className={classes.icon + ' ' + classes.secondIcon}
+          icon={faQrcode}
+        />
       </div>
     </div>
   );
--- src/pages/Professor/Exercises.js
+++ src/pages/Professor/Exercises.js
@@ -1,7 +1,46 @@
+import Exercise from '../../components/Body/professors/LectureExercise';
+import ButtonBordered from '../../components/UI/Buttons/ButtonBordered';
+
+import classes from './Exercises.module.css';
+
+const DUMMY_EXERCISES = [
+  {
+    index: '1',
+    name: 'Uvod u baze podataka',
+    duration: '3',
+    date: '20.02.2023',
+  },
+  {
+    index: '2',
+    name: 'Razvojni ciklus baze podataka',
+    duration: '3',
+    date: '27.02.2023',
+  },
+];
+
 const ExercisesPage = () => {
   return (
-    <div>
-      <h1>Vježbe</h1>
+    <div className={classes.exercisesPage}>
+      <h2 className={classes.title}>Vježbe</h2>
+      <div className={classes.exercisesInfo}>
+        <p>Broj održanih vježbi: 2</p>
+        <p>Preostali broj časova: 39</p>
+      </div>
+      <div className={classes.exercises}>
+        <div className={classes.tableInfo}>
+          <p className={classes.exerciseIndex}>RB</p>
+          <p className={classes.exerciseName}>Naslov vježbi</p>
+          <p className={classes.exerciseDuration}>broj časova</p>
+          <p className={classes.exerciseDate}>Datum vježbi</p>
+          <p className={classes.exerciseScan}>Skeniranje</p>
+          <p className={classes.exerciseQR}>QR</p>
+        </div>
+        <div className={classes.tableInfoLine}></div>
+        {DUMMY_EXERCISES.map((exercise) => (
+          <Exercise key={exercise.index} info={exercise} />
+        ))}
+      </div>
+      <ButtonBordered className={classes.button}>+</ButtonBordered>
     </div>
   );
 };
